f20b4d6c4ccc5571539683f22dff64fa9cd75e93
Aishal Gupta <aishalgupta3008@gmail.com>
2022-05-07 16:14:04 +0530

    Add files via upload

---
+++ loan_flipr/Loan-Company-Responsive-Website-Templates/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <Header></Header>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap');
+</style>
